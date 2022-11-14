4a291db0c192bd37cedf62dfca2e0b865b7a7d10
Bashar <Bashar.zureikat@gmail.com>
2022-11-14 21:59:10 +0000

    done

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-back-end-heroku.herokuapp.com/");
 
 function App() {
   const [username, setUsername] = useState("");
@@ -21,26 +22,36 @@ function App() {
     <>
       <div className="App">
         {showForm ? (
-          <div className="joinChatContainer">
-            <h3>Join A Chat</h3>
-            <input
-              type="text"
-              placeholder="John..."
-              onChange={(event) => {
-                setUsername(event.target.value);
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Room ID..."
-              onChange={(event) => {
-                setRoom(event.target.value);
-              }}
-            />
-            <button class="joinroombtn" onClick={joinRoom}>
-              Join A Room
-            </button>
-          </div>
+          <Box
+            shadow="dark-lg"
+            border="2px"
+            w="20%"
+            h="280px"
+            bgGradient="linear(to-t, purple.600, purple.200)"
+          >
+            <div className="joinChatContainer">
+            <Flex flexDir="column" align="center" justify="center">
+              <Text fontSize="5xl" textShadow='1px 1px black' color="white">Join A Chat</Text>
+              <input
+                type="text"
+                placeholder="John..."
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+              <input
+                type="text"
+                placeholder="Room ID..."
+                onChange={(event) => {
+                  setRoom(event.target.value);
+                }}
+              />
+              <Button shadow="dark-lg" margin="5" padding="5" class="joinroombtn" onClick={joinRoom}>
+                Join A Room
+              </Button>
+            </Flex>
+            </div>
+          </Box>
         ) : (
           <Chat socket={socket} username={username} room={room} />
         )}
